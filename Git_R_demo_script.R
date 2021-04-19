8259b17954c47902a2771b2ab5716d1d2ac8b6ee
Stephanie Labou <slabou@ucsd.edu>
2021-04-19 09:54:38 -0700

    First commit

---
+++ Git_R_demo_script.R
@@ -0,0 +1,10 @@
+#R script for Git/R demo
+
+#Make a dataframe
+df<-data.frame(y=rnorm(10),x1=rnorm(10),x2=rnorm(10))
+
+#simple regression
+lm(y~x1 + x2, data = df)
+
+#same result, different syntax
+lm(y~.,df)
